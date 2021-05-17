cda70ec99f3882a2461a6083a209c17b9e586d8c
radu81 <radu_81@hotmail.com>
2021-05-17 14:42:50 +0200

    Update LevGal-ModLog.italian.php

--- LevGal-ModLog.italian.php
+++ LevGal-ModLog.italian.php
@@ -10,21 +10,21 @@
  */
 // Version: 1.0; LevGal moderation log
 
-$txt['levgal_modlog'] = 'Moderation Log';
+$txt['levgal_modlog'] = 'Log moderazione';
 
-$txt['lgal_modlog_album_deleted'] = '(deleted album, id %1$s)';
-$txt['lgal_modlog_item_deleted'] = '(deleted item, id %1$s)';
+$txt['lgal_modlog_album_deleted'] = '(album eliminato, id %1$s)';
+$txt['lgal_modlog_item_deleted'] = '(elemento eliminato, id %1$s)';
 
-$txt['lgal_ev_approve_album'] = 'Approved album: {album_link}';
-$txt['lgal_ev_delete_album'] = 'Deleted album: {album_name} (items deleted: {items_deleted})';
-$txt['lgal_ev_feature_album'] = 'Marked album featured: {album_link}';
-$txt['lgal_ev_unfeature_album'] = 'Marked album unfeatured: {album_link}';
+$txt['lgal_ev_approve_album'] = 'Album approvato: {album_link}';
+$txt['lgal_ev_delete_album'] = 'Album eliminato: {album_name} (elementi eliminati: {items_deleted})';
+$txt['lgal_ev_feature_album'] = 'Album contrassegnato come in evidenza: {album_link}';
+$txt['lgal_ev_unfeature_album'] = 'Album contrassegnato come non in evidenza: {album_link}';
 
-$txt['lgal_ev_approve_item'] = 'Approved item: {item_link}';
-$txt['lgal_ev_unapprove_item'] = 'Unapproved item: {item_link}';
-$txt['lgal_ev_delete_item'] = 'Deleted item: {item_name}';
-$txt['lgal_ev_move_item'] = 'Moved: {item_link} from {old_album} to {album_link}';
+$txt['lgal_ev_approve_item'] = 'Elemento approvato: {item_link}';
+$txt['lgal_ev_unapprove_item'] = 'Elemento non approvato: {item_link}';
+$txt['lgal_ev_delete_item'] = 'Elemento eliminato: {item_name}';
+$txt['lgal_ev_move_item'] = 'Spostato: {item_link} da {old_album} a {album_link}';
 
-$txt['lgal_ev_approve_comment_deleted'] = 'Approved comment to item: {item_link}';
-$txt['lgal_ev_approve_comment'] = 'Approved <a href="{comment_url}">comment</a> to item: {item_link}';
-$txt['lgal_ev_delete_comment'] = 'Deleted comment to item: {item_link}';
+$txt['lgal_ev_approve_comment_deleted'] = 'Commento approvato in: {item_link}';
+$txt['lgal_ev_approve_comment'] = '<a href="{comment_url}">Commento</a> approvato in: {item_link}';
+$txt['lgal_ev_delete_comment'] = 'Commento eliminato: {item_link}';
