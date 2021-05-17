fce1f33239d4d7f4f1fe8e91bc0c71dc16f8f210
radu81 <radu_81@hotmail.com>
2021-05-17 14:42:04 +0200

    Update LevGal-Errors.italian.php

--- LevGal-Errors.italian.php
+++ LevGal-Errors.italian.php
@@ -13,45 +13,45 @@
 // Important! Before editing these language files please read the text at the top of index.english.php.
 
 // General errors
-$txt['levgal_invalid_action'] = 'Sorry, the requested page could not be found.';
-$txt['levgal_no_image_support'] = 'Sorry, no valid support was found for resizing images.';
-$txt['error_no_album'] = 'Sorry, no album was found. It may be off limits or have been removed.';
-$txt['error_no_item'] = 'Sorry, no item was found. It may be off limits or have been removed.';
-$txt['error_no_tags'] = 'Sorry, no items that you can see have been tagged with anything.';
-$txt['levgal_gallery_over_quota'] = 'The media gallery has a limit of %1$s of files to be uploaded. More cannot be added until back under that quota, or the administrator changes the quota.';
+$txt['levgal_invalid_action'] = 'Spiacenti, la pagine richiesta non è stata trovata.';
+$txt['levgal_no_image_support'] = 'Spiacenti, non è stato trovato alcun supporto valido per il ridimensionamento delle immagini.';
+$txt['error_no_album'] = 'Spiacenti, non è stato trovato alcun album. Potrebbe essere non accessibile o rimosso .';
+$txt['error_no_item'] = 'Spiacenti, non è stato trovato alcun elemento. Potrebbe essere non accessibile o rimosso.';
+$txt['error_no_tags'] = 'Spiacenti, nessun elemento che puoi vedere è stato taggato con qualcosa.';
+$txt['levgal_gallery_over_quota'] = 'La galleria multimediale ha un limite di %1$s file da caricare. Non è possibile aggiungerne altri finché non si torna al di sotto di quella quota o l\'amministratore non modifica la quota.';
 
 // Permission dependent
-$txt['cannot_lgal_view'] = 'Sorry, you are not permitted to see the media gallery.';
-$txt['cannot_lgal_moderate'] = 'Sorry, you are not permitted to see the media gallery moderation area.';
+$txt['cannot_lgal_view'] = 'Spiacenti, non sei autorizzato a visualizzare la galleria multimediale.';
+$txt['cannot_lgal_moderate'] = 'Spiacenti, non sei autorizzato a vedere l\'area moderazione della galleria multimediale.';
 
-$txt['cannot_lgal_addalbum'] = 'Sorry, you are not permitted to create a new album.';
-$txt['cannot_lgal_approve_album'] = 'Sorry, you are not permitted to approve this album.';
-$txt['cannot_lgal_edit_album'] = 'Sorry, you are not permitted to edit this album.';
-$txt['cannot_lgal_delete_album'] = 'Sorry, you are not permitted to delete this album.';
+$txt['cannot_lgal_addalbum'] = 'Spiacenti, non sei autorizzato a creare un nuovo albun.';
+$txt['cannot_lgal_approve_album'] = 'Spiacenti, non sei autorizzato ad approvare questo album.';
+$txt['cannot_lgal_edit_album'] = 'Spiacenti, non sei autorizzato a modificare questo album.';
+$txt['cannot_lgal_delete_album'] = 'Spiacenti, non sei autorizzato a cancellare questo album.';
 
-$txt['cannot_lgal_additem'] = 'Sorry, you are not permitted to add a new item here.';
-$txt['cannot_lgal_addbulk'] = 'Sorry, you are not permitted to upload items in bulk.';
-$txt['cannot_lgal_approve_item'] = 'Sorry, you are not permitted to approve this item.';
-$txt['cannot_lgal_edit_item'] = 'Sorry, you are not permitted to edit this item.';
-$txt['cannot_lgal_delete_item'] = 'Sorry, you are not permitted to delete this item.';
-$txt['cannot_lgal_move_item'] = 'Sorry, you are not permitted to move this item.';
+$txt['cannot_lgal_additem'] = 'Spiacenti, non sei autorizzato ad aggiungere nuovi elementi qui.';
+$txt['cannot_lgal_addbulk'] = 'Spiacenti, non sei autorizzato a caricare elementi in massa.';
+$txt['cannot_lgal_approve_item'] = 'Spiacenti, non sei autorizzato ad approvare questo elemento.';
+$txt['cannot_lgal_edit_item'] = 'Spiacenti, non sei autorizzato a modificare questo elemento.';
+$txt['cannot_lgal_delete_item'] = 'Spiacenti, non sei autorizzato a cancellare questo elemento.';
+$txt['cannot_lgal_move_item'] = 'Spiacenti, non sei autorizzato a spostare questo elemento.';
 
-$txt['cannot_lgal_add_comment'] = 'Sorry, you are not permitted to comment on this item.';
-$txt['cannot_lgal_approve_comment'] = 'Sorry, you are not permitted to approve that comment.';
-$txt['cannot_lgal_edit_comment'] = 'Sorry, you are not permitted to edit that comment.';
-$txt['cannot_lgal_delete_comment'] = 'Sorry, you are not permitted to delete that comment.';
+$txt['cannot_lgal_add_comment'] = 'Spiacenti, non sei autorizzato a commentare questo elemento.';
+$txt['cannot_lgal_approve_comment'] = 'Spiacenti, non sei autorizzato ad approvare questo commento.';
+$txt['cannot_lgal_edit_comment'] = 'Spiacenti, non sei autorizzato a modificare questo commento.';
+$txt['cannot_lgal_delete_comment'] = 'Spiacenti, non sei autorizzato a cancellare questo commento.';
 
-$txt['cannot_lgal_flag_item'] = 'Sorry, you are not permitted to report items to the moderators.';
-$txt['cannot_lgal_flag_comment'] = 'Sorry, you are not permitted to report comments to the moderators.';
-$txt['cannot_lgal_setthumbnail'] = 'Sorry, you are not permitted to use this item as album thumbnail.';
+$txt['cannot_lgal_flag_item'] = 'Spiacenti, non sei autorizzato a segnalare elementi ai moderatori.';
+$txt['cannot_lgal_flag_comment'] = 'Spiacenti, non sei autorizzato a segnalare commenti ai moderatori.';
+$txt['cannot_lgal_setthumbnail'] = 'Spiacenti, non sei autorizzato ad utilizzare questo elemento come miniatura dell\'album.';
 
-$txt['cannot_lgal_feed'] = 'Sorry, this feed is disabled.';
+$txt['cannot_lgal_feed'] = 'Spiacenti, questo feed è disabilitato.';
 
 // Situation dependent
-$txt['lgal_no_album_destination'] = 'Sorry, you cannot move this item; there are no other albums to move it to.';
-$txt['lgal_missing_required_field'] = 'You need to fill in the field marked "%1$s".';
-$txt['lgal_invalid_number_range'] = 'The field marked "%1$s" is required to be a number between %2$s and %3$s.';
-$txt['lgal_string_too_long'] = 'The field marked "%1$s" is too long; only %2$s characters are allowed.';
-$txt['lgal_invalid_email_field'] = 'The field marked "%1$s" is required to be an email address.';
-$txt['lgal_invalid_numbers_field'] = 'The field marked "%1$s" is required to be all numeric digits (0-9)';
-$txt['lgal_invalid_field'] = 'The field marked "%1$s" is not valid.';
+$txt['lgal_no_album_destination'] = 'Spiacenti, non puoi spostare questo elemento; non ci sono altri album in cui spostarlo.';
+$txt['lgal_missing_required_field'] = 'È necessario compilare il campo contrassegnato con "%1$s".';
+$txt['lgal_invalid_number_range'] = 'Il campo contrassegnato con "%1$s" deve essere un numero compreso tra %2$s e %3$s.';
+$txt['lgal_string_too_long'] = 'Il campo contrassegnato con "%1$s" è troppo lungo; solamente %2$s charatteri sono permessi.';
+$txt['lgal_invalid_email_field'] = 'Il campo contrassegnato con "%1$s" deve essere un indirizzo email.';
+$txt['lgal_invalid_numbers_field'] = 'Il campo contrassegnato con "%1$s" deve contenere solo cifre numeriche (0-9)';
+$txt['lgal_invalid_field'] = 'Il campo contrassegnato con "%1$s" non è valido.';
