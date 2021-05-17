0081d7e3cb1a8d201ea78e48136f6b13242ad32c
radu81 <radu_81@hotmail.com>
2021-05-17 15:31:53 +0200

    Update LevGal-Upload.italian.php

--- LevGal-Upload.italian.php
+++ LevGal-Upload.italian.php
@@ -13,48 +13,48 @@
 // Important! Before editing these language files please read the text at the top of index.english.php.
 
 // General adding/editing:
-$txt['lgal_adding_to_album'] = 'Uploading to album: %1$s';
-$txt['lgal_editing_item'] = 'Editing item: %1$s';
-$txt['lgal_item_slug'] = 'Item slug:';
-$txt['lgal_item_description'] = 'Description for this item (you don\'t have to put this in):';
+$txt['lgal_adding_to_album'] = 'Caricamento nell\'album: %1$s';
+$txt['lgal_editing_item'] = 'Modifica dell\'elemento: %1$s';
+$txt['lgal_item_slug'] = 'Slug elemento:';
+$txt['lgal_item_description'] = 'Descrizione di questo elemento (non necessaria):';
 
 // Quota strings:
-$txt['lgal_allowed_formats'] = '<u>You can add the following types:</u>';
-$txt['lgal_allowed_format_image'] = '<em><b>Images</b></em>: %1$s';
+$txt['lgal_allowed_formats'] = '<u>Puoi aggiungere i seguenti tipi:</u>';
+$txt['lgal_allowed_format_image'] = '<em><b>Immagini</b></em>: %1$s';
 $txt['lgal_allowed_format_audio'] = '<em><b>Audio</b></em>: %1$s';
 $txt['lgal_allowed_format_video'] = '<em><b>Video</b></em>: %1$s';
-$txt['lgal_allowed_format_document'] = '<em><b>Documents</b></em>: %1$s';
-$txt['lgal_allowed_format_archive'] = '<em><b>Archives</b></em>: %1$s';
-$txt['lgal_allowed_format_generic'] = '<em><b>Others</b></em>: %1$s';
-$txt['lgal_allowed_external'] = 'You can add items from: %1$s';
+$txt['lgal_allowed_format_document'] = '<em><b>Documenti</b></em>: %1$s';
+$txt['lgal_allowed_format_archive'] = '<em><b>Archivi</b></em>: %1$s';
+$txt['lgal_allowed_format_generic'] = '<em><b>Altro</b></em>: %1$s';
+$txt['lgal_allowed_external'] = 'Puoi aggiungere elementi da: %1$s';
 
 // Single uploading:
-$txt['lgal_item_name_and_slug_auto'] = 'Item name and slug will be created for you when adding a file, or you may enter a custom one.';
-$txt['lgal_item_press_browse'] = 'To upload, press the browse button - ';
-$txt['lgal_item_drag_here'] = 'Drag a file here to upload, or click to select one.';
-$txt['lgal_item_browse'] = 'Browse...';
-$txt['lgal_item_want_to_add_file'] = 'I want to add a file';
-$txt['lgal_item_want_to_edit_file'] = 'I want to change the file';
-$txt['lgal_item_want_to_add_link'] = 'I want to link to a website';
+$txt['lgal_item_name_and_slug_auto'] = 'Il nome e lo slug dell\'elemento verranno creati per te quando aggiungi un file, oppure puoi inserirne uno personalizzato.';
+$txt['lgal_item_press_browse'] = 'Per caricare, premere il pulsante Sfoglia - ';
+$txt['lgal_item_drag_here'] = 'Trascina un file qui per caricarlo o fai clic per selezionarne uno.';
+$txt['lgal_item_browse'] = 'Sfoglia...';
+$txt['lgal_item_want_to_add_file'] = 'Voglio aggiungere un file ';
+$txt['lgal_item_want_to_edit_file'] = 'Voglio cambiare un file ';
+$txt['lgal_item_want_to_add_link'] = 'Voglio aggiungere un collegamento ad un sito web';
 
 // Bulk uploading:
-$txt['lgal_item_add_to_queue'] = 'Add Files...';
-$txt['lgal_item_remove_from_queue'] = 'Remove file';
-$txt['lgal_item_add_to_queue_desc'] = 'To upload files, press the add button - ';
-$txt['lgal_item_drag_here_multiple'] = 'Drag file(s) here to upload, or click to select them';
-$txt['lgal_file_name'] = 'File name:';
-$txt['lgal_begin_upload'] = 'Begin Upload';
-$txt['lgal_queue_empty'] = 'There are no files to be uploaded right now.';
-$txt['lgal_processing'] = 'Processing...';
+$txt['lgal_item_add_to_queue'] = 'Aggiungi file...';
+$txt['lgal_item_remove_from_queue'] = 'Rimuovi file';
+$txt['lgal_item_add_to_queue_desc'] = 'Per caricare i file, premere il pulsante Aggiungi - ';
+$txt['lgal_item_drag_here_multiple'] = 'Trascina i file qui per caricarli o fai clic per selezionarli ';
+$txt['lgal_file_name'] = 'Nome file';
+$txt['lgal_begin_upload'] = 'Inizia il caricamento';
+$txt['lgal_queue_empty'] = 'Non ci sono file da caricare in questo momento.';
+$txt['lgal_processing'] = 'In lavorazione...';
 
 // Additional options:
-$txt['lgal_additional_options'] = 'Additional Options';
-$txt['lgal_mark_mature'] = 'Mark item as mature?';
-$txt['lgal_enable_comments'] = 'Enable comments on this item?';
-$txt['lgal_enable_notify'] = 'Notify me of comments on this item?';
-$txt['lgal_item_is_approved'] = 'Mark item as approved?';
+$txt['lgal_additional_options'] = 'Ozioni aggiuntive';
+$txt['lgal_mark_mature'] = 'Contrassegna l\'elemento come contenuto per adulti?';
+$txt['lgal_enable_comments'] = 'Abilitare i commenti su questo elemento?';
+$txt['lgal_enable_notify'] = 'Notificami i commenti su questo elemento?';
+$txt['lgal_item_is_approved'] = 'Contrassegnare l\'elemento come approvato?';
 
-$txt['lgal_edit_item_comments'] = 'Item comments:';
-$txt['lgal_comments_enabled'] = 'Allowed';
-$txt['lgal_comments_no_new'] = 'No new comments allowed';
-$txt['lgal_comments_disabled'] = 'No comments, all hidden';
+$txt['lgal_edit_item_comments'] = 'Commenti degli elementi:';
+$txt['lgal_comments_enabled'] = 'Consentiti';
+$txt['lgal_comments_no_new'] = 'Non sono ammessi nuovi commenti';
+$txt['lgal_comments_disabled'] = 'Nessun commento, tutto nascosto';
